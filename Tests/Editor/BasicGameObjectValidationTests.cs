83cd33ac00fabeb23eaefb6cb63495b26bacbc7e
Darren Tsung <darren.tsung@gmail.com>
2017-05-27 14:49:20 -0700

    Test that missing / filled outlets work as expected

--- Tests/Editor/BasicGameObjectValidationTests.cs
+++ Tests/Editor/BasicGameObjectValidationTests.cs
@@ -19,5 +19,32 @@ namespace DTValidator.Internal {
 			IList<IValidationError> errors = Validator.Validate(emptyGameObject);
 			Assert.That(errors, Is.Null);
 		}
+
+		private class OutletComponent : MonoBehaviour {
+			public GameObject Outlet;
+		}
+
+		[Test]
+		public static void FilledOutlet_ReturnsNoErrors() {
+			GameObject gameObject = new GameObject();
+
+			var outletComponent = gameObject.AddComponent<OutletComponent>();
+			outletComponent.Outlet = gameObject;
+
+			IList<IValidationError> errors = Validator.Validate(gameObject);
+			Assert.That(errors, Is.Null);
+		}
+
+		[Test]
+		public static void MissingOutlet_ReturnsErrors() {
+			GameObject gameObject = new GameObject();
+
+			var outletComponent = gameObject.AddComponent<OutletComponent>();
+			outletComponent.Outlet = null;
+
+			IList<IValidationError> errors = Validator.Validate(gameObject);
+			Assert.That(errors, Is.Not.Null);
+			Assert.That(errors.Count, Is.EqualTo(1));
+		}
 	}
 }
