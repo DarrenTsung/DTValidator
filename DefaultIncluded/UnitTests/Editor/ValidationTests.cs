ddf182ee55e3268e6c85e70a18aef75864258912
Darren Tsung <darren.tsung@gmail.com>
2017-06-08 08:42:22 -0700

    Move included assets to folder named “DefaultIncluded” add validator unit tests

---
+++ DefaultIncluded/UnitTests/Editor/ValidationTests.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Events;
+
+using NUnit.Framework;
+
+namespace DTValidator {
+	public static class ValidationTests {
+		[Test]
+		public static void ValidateSavedScenes() {
+			IList<IValidationError> errors = ValidationUtil.ValidateAllGameObjectsInSavedScenes(earlyExitOnError: true);
+			Assert.That(errors, Is.Empty);
+		}
+
+		[Test]
+		public static void ValidateSavedScriptableObjects() {
+			IList<IValidationError> errors = ValidationUtil.ValidateAllSavedScriptableObjects(earlyExitOnError: true);
+			Assert.That(errors, Is.Empty);
+		}
+
+		[Test]
+		public static void ValidateGameObjectsInResources() {
+			IList<IValidationError> errors = ValidationUtil.ValidateAllGameObjectsInResources(earlyExitOnError: true);
+			Assert.That(errors, Is.Empty);
+		}
+	}
+}
