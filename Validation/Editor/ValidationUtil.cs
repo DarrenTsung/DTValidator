b0d4c7f5a9e703a62af8ed707fc59424c2b3199d
Javier Bullrich <javier.bullrich@innerspace.eu>
2020-06-05 15:39:49 +0200

    Changed batch mode for application call
    I also removed all the unused imports

--- Validation/Editor/ValidationUtil.cs
+++ Validation/Editor/ValidationUtil.cs
@@ -1,13 +1,8 @@
 #if UNITY_EDITOR
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Reflection;
 using UnityEngine;
-using UnityEngine.Events;
-
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine.SceneManagement;
@@ -84,7 +79,7 @@ namespace DTValidator {
 
 		public static bool HasToPersistScene()
 		{
-			if (UnityEditorInternal.InternalEditorUtility.inBatchMode)
+			if (Application.isBatchMode)
 			{
 				EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Single);
 				return false;
